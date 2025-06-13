48cba53be55c452806259566b1910918d2d9862c
Taein5415 <kskk5050@naver.com>
2025-06-13 10:14:33 +0900

    [init] Department 엔티티 수정

--- momentum-dao-be/src/main/java/com/dao/momentum/organization/department/command/domain/aggregate/Department.java
+++ momentum-dao-be/src/main/java/com/dao/momentum/organization/department/command/domain/aggregate/Department.java
@@ -1,10 +1,7 @@
 package com.dao.momentum.organization.department.command.domain.aggregate;
 
 import com.dao.momentum.organization.department.command.domain.aggregate.IsDeleted;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,12 +16,20 @@ public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer deptId;
+
     private Integer deptHeadId;
+
     private Integer parentDeptId;
+
     private String name;
+
     private String contact;
+
     private LocalDateTime createdAt = LocalDateTime.now();
+
     private LocalDateTime updatedAt = LocalDateTime.now();
+
+    @Enumerated(EnumType.STRING)
     private IsDeleted isDeleted = IsDeleted.N;
 
 }
