9e1deaa0541822cdaf01ea69ef4f39c70f9939f9
Taein5415 <kskk5050@naver.com>
2025-06-16 14:17:06 +0900

    [init] IpAddress 엔티티 수정

--- momentum-dao-be/src/main/java/com/dao/momentum/organization/company/command/domain/aggregate/IpAddress.java
+++ momentum-dao-be/src/main/java/com/dao/momentum/organization/company/command/domain/aggregate/IpAddress.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +18,9 @@ public class IpAddress {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer ipAddressId;
 
-    @NotNull
+    @NotBlank
     private String ipAddress;
+
+    @NotBlank
+    private String ipName;
 }
