a441454624074f47d5ad86383ba65d3c3d9f232c
Taein5415 <kskk5050@naver.com>
2025-06-16 10:49:18 +0900

    [init] Contract 엔티티 수정

--- momentum-dao-be/src/main/java/com/dao/momentum/organization/contract/command/domain/aggregate/Contract.java
+++ momentum-dao-be/src/main/java/com/dao/momentum/organization/contract/command/domain/aggregate/Contract.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -21,10 +22,12 @@ public class Contract {
 
     private Long empId;
 
+    @Enumerated(EnumType.STRING)
     private ContractType type;
 
     @Column(precision = 15, scale = 3)
     private BigDecimal salary;
 
+    @CreatedDate
     private LocalDate createdAt;
 }
