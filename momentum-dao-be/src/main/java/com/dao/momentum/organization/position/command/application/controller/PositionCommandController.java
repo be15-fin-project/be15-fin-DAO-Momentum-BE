1d345fa43cf9b8ae0bad93a62d17c98668e1071d
Taein5415 <kskk5050@naver.com>
2025-06-17 09:43:43 +0900

    [feat #102] 직위 정보 수정 기능 구현

--- momentum-dao-be/src/main/java/com/dao/momentum/organization/position/command/application/controller/PositionCommandController.java
+++ momentum-dao-be/src/main/java/com/dao/momentum/organization/position/command/application/controller/PositionCommandController.java
@@ -2,7 +2,9 @@ package com.dao.momentum.organization.position.command.application.controller;
 
 import com.dao.momentum.common.dto.ApiResponse;
 import com.dao.momentum.organization.position.command.application.dto.request.PositionCreateRequest;
+import com.dao.momentum.organization.position.command.application.dto.request.PositionUpdateRequest;
 import com.dao.momentum.organization.position.command.application.dto.response.PositionCreateResponse;
+import com.dao.momentum.organization.position.command.application.dto.response.PositionUpdateResponse;
 import com.dao.momentum.organization.position.command.application.service.PositionCommandService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,9 +24,16 @@ public class PositionCommandController {
 
     @Operation(summary = "직위 생성", description = "관리자는 회사의 직위를 생성할 수 있다.")
     @PostMapping
-    public ResponseEntity<ApiResponse<PositionCreateResponse>> createPositions(@RequestBody PositionCreateRequest request){
+    public ResponseEntity<ApiResponse<PositionCreateResponse>> createPosition(@RequestBody PositionCreateRequest request){
         PositionCreateResponse positionsResponse = positionService.createPositions(request);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(positionsResponse));
+    }
+
+    @Operation(summary = "직위 수정", description = "관리자는 회사의 직위를 수정할 수 있다.")
+    @PutMapping
+    public ResponseEntity<ApiResponse<PositionUpdateResponse>> updatePosition(@RequestBody PositionUpdateRequest request){
+        PositionUpdateResponse response = positionService.updatePosition(request);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(response));
     }
 
 }
--- momentum-dao-be/src/main/java/com/dao/momentum/organization/position/command/application/service/PositionCommandService.java
+++ momentum-dao-be/src/main/java/com/dao/momentum/organization/position/command/application/service/PositionCommandService.java
@@ -2,17 +2,21 @@ package com.dao.momentum.organization.position.command.application.service;
 
 import com.dao.momentum.common.exception.ErrorCode;
 import com.dao.momentum.organization.position.command.application.dto.request.PositionCreateRequest;
+import com.dao.momentum.organization.position.command.application.dto.request.PositionUpdateRequest;
 import com.dao.momentum.organization.position.command.application.dto.response.PositionCreateResponse;
+import com.dao.momentum.organization.position.command.application.dto.response.PositionUpdateResponse;
 import com.dao.momentum.organization.position.command.domain.aggregate.Position;
 import com.dao.momentum.organization.position.command.domain.repository.PositionRepository;
 import com.dao.momentum.organization.position.exception.PositionException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PositionCommandService {
     private final PositionRepository positionRepository;
     private final ModelMapper modelMapper;
@@ -23,8 +27,10 @@ public class PositionCommandService {
             throw new PositionException(ErrorCode.POSITION_ALREADY_EXISTS);
         }
 
-        //POSITION_ALREADY_EXISTS
         Integer requestedLevel = request.getLevel();
+
+        //직위 단계 범위검사
+        validateLevelForCreate(requestedLevel);
 
         // 이미 존재하는 레벨인지 확인
         boolean levelExists = positionRepository.existsByLevel(requestedLevel);
@@ -36,6 +42,56 @@ public class PositionCommandService {
         Position position = modelMapper.map(request, Position.class);
         positionRepository.save(position);
 
+
         return new PositionCreateResponse(position.getPositionId(), "직위가 생성되었습니다.");
+    }
+
+    @Transactional
+    public PositionUpdateResponse updatePosition(PositionUpdateRequest request) {
+        Integer requestedLevel = request.getLevel();
+
+        //직위 단계 범위검사
+        validateLevelForUpdate(requestedLevel);
+
+        //직위 존재 검사
+        Position position = positionRepository.findByPositionId(request.getPositionId()).orElseThrow(
+                () ->  new PositionException(ErrorCode.POSITION_NOT_FOUND)
+        );
+
+        //직위명 검사
+        if(!position.getName().equals(request.getName()) && positionRepository.existsByName(request.getName())){
+            throw new PositionException(ErrorCode.POSITION_ALREADY_EXISTS);
+        }
+
+        //직위 단계 조정
+        if(position.getLevel()>requestedLevel){
+            positionRepository.incrementLevelsInRange(requestedLevel, position.getLevel()-1);
+        }
+        else if(position.getLevel() < requestedLevel){
+            positionRepository.decrementLevelsInRange(position.getLevel()+1, requestedLevel);
+        }
+
+        position.setName(request.getName());
+
+        position.setLevel(requestedLevel);
+
+        return new PositionUpdateResponse(position.getPositionId(), "직위가 수정되었습니다.");
+    }
+
+    private void validateLevelForCreate(int requestLevel) {
+        Integer maxLevel = positionRepository.findMaxLevel();
+        if (maxLevel == null) maxLevel = 0;
+
+        if (requestLevel < 1 || requestLevel > maxLevel + 1) {
+            throw new PositionException(ErrorCode.INVALID_LEVEL);
+        }
+    }
+
+    private void validateLevelForUpdate(int requestLevel) {
+        Integer maxLevel = positionRepository.findMaxLevel();
+
+        if (maxLevel == null || requestLevel < 1 || requestLevel > maxLevel) {
+            throw new PositionException(ErrorCode.INVALID_LEVEL);
+        }
     }
 }
