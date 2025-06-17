01ae779475159cee5e802beaa6bd7748413a46b2
splguyjr <splguyjr@naver.com>
2025-06-17 10:44:22 +0900

    [feat #73] 공지사항 삭제 controller 테스트 코드 작성 및 @WithMockUser 로직 추가

--- momentum-dao-be/src/test/java/com/dao/momentum/announcement/command/application/controller/AnnouncementCommandControllerTest.java
+++ momentum-dao-be/src/test/java/com/dao/momentum/announcement/command/application/controller/AnnouncementCommandControllerTest.java
@@ -14,18 +14,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(AnnouncementCommandController.class)
 @AutoConfigureMockMvc(addFilters = false)
+@WithMockUser(username = "123")
 class AnnouncementCommandControllerTest {
 
     @Autowired
@@ -52,7 +56,7 @@ class AnnouncementCommandControllerTest {
                 "files", "test.txt", "text/plain", "내용".getBytes());
 
         AnnouncementCreateResponse response = new AnnouncementCreateResponse(1L);
-        when(announcementCommandService.create(any(), any(), any())).thenReturn(response);
+        when(announcementCommandService.create(any(), any(), any(UserDetails.class))).thenReturn(response);
 
         // when & then
         mockMvc.perform(multipart("/announcement")
@@ -72,7 +76,7 @@ class AnnouncementCommandControllerTest {
         request.setRemainFileIdList(List.of(1L, 2L));
 
         AnnouncementModifyResponse response = new AnnouncementModifyResponse(1L);
-        when(announcementCommandService.modify(any(), any(), eq(1L), any())).thenReturn(response);
+        when(announcementCommandService.modify(any(), any(), eq(1L), any(UserDetails.class))).thenReturn(response);
 
         MockMultipartFile jsonPart = new MockMultipartFile(
                 "announcement", null, "application/json", objectMapper.writeValueAsBytes(request));
@@ -89,13 +93,30 @@ class AnnouncementCommandControllerTest {
     }
 
     @Test
-    @DisplayName("공지사항이 존재하지 않으면 404")
+    @DisplayName("공지사항 삭제 성공")
+    void testDeleteAnnouncement() throws Exception {
+        // given
+        Long announcementId = 1L;
+
+        doNothing().when(announcementCommandService).delete(eq(announcementId), any(UserDetails.class));
+
+        // when & then
+        mockMvc.perform(delete("/announcement/{announcementId}", announcementId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data").doesNotExist());
+
+        verify(announcementCommandService).delete(eq(announcementId), any(UserDetails.class));
+    }
+
+    @Test
+    @DisplayName("공지사항 수정 - 공지사항이 존재하지 않으면 404")
     void testModifyAnnouncement_NotFound() throws Exception {
         AnnouncementModifyRequest request = new AnnouncementModifyRequest();
         request.setTitle("제목");
         request.setContent("내용");
 
-        when(announcementCommandService.modify(any(), any(), eq(999L), any()))
+        when(announcementCommandService.modify(any(), any(), eq(999L), any(UserDetails.class)))
                 .thenThrow(new NoSuchAnnouncementException(ErrorCode.ANNOUNCEMENT_NOT_FOUND));
 
         MockMultipartFile jsonPart = new MockMultipartFile(
