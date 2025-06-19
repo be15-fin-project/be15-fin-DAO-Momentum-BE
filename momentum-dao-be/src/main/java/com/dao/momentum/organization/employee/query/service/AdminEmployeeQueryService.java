2004d3359af0572fd136f26ec1724c6a9d517d1c
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-06-19 10:37:39 +0900

    [feat #34] 사원 전체 목록 조회 기능 구현
    - 사원 목록 조회 기능 구현

---
+++ momentum-dao-be/src/main/java/com/dao/momentum/organization/employee/query/service/AdminEmployeeQueryService.java
@@ -0,0 +1,42 @@
+package com.dao.momentum.organization.employee.query.service;
+
+import com.dao.momentum.common.dto.Pagination;
+import com.dao.momentum.organization.employee.query.dto.request.EmployeeSearchDTO;
+import com.dao.momentum.organization.employee.query.dto.request.EmployeeSearchRequest;
+import com.dao.momentum.organization.employee.query.dto.response.EmployeeListResponse;
+import com.dao.momentum.organization.employee.query.dto.response.EmployeeSummaryDTO;
+import com.dao.momentum.organization.employee.query.mapper.AdminEmployeeMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AdminEmployeeQueryService {
+
+    private final AdminEmployeeMapper adminEmployeeMapper;
+
+    public EmployeeListResponse getEmployees(EmployeeSearchRequest employeeSearchRequest) {
+        EmployeeSearchDTO employeeSearchDTO = EmployeeSearchRequest.fromRequest(employeeSearchRequest);
+
+        List<EmployeeSummaryDTO> employees = adminEmployeeMapper.getEmployees(employeeSearchDTO);
+
+        int page = employeeSearchDTO.getPage();
+        int size = employeeSearchDTO.getSize();
+
+        long totalItems = adminEmployeeMapper.countEmployees(employeeSearchDTO);
+        int totalPages = (int) Math.ceil((double) totalItems / size);
+
+        return EmployeeListResponse.builder()
+                .employees(employees)
+                .pagination(
+                        Pagination.builder()
+                                .currentPage(page)
+                                .totalItems(totalItems)
+                                .totalPage(totalPages)
+                                .build()
+                )
+                .build();
+    }
+}
