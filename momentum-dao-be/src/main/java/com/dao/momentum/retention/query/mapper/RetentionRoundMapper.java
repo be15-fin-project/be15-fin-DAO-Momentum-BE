856f70b937fa35474ebda88bd0ea1515862469e3
meowdule <meowdule73@gmail.com>
2025-06-23 15:32:38 +0900

    [feat  #92] 근속 전망 회차 조회 기능 구현

---
+++ momentum-dao-be/src/main/java/com/dao/momentum/retention/query/mapper/RetentionRoundMapper.java
@@ -0,0 +1,18 @@
+package com.dao.momentum.retention.query.mapper;
+
+import com.dao.momentum.retention.query.dto.request.RetentionRoundRawDto;
+import com.dao.momentum.retention.query.dto.request.RetentionRoundSearchRequestDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface RetentionRoundMapper {
+
+    // 회차 목록 조회
+    List<RetentionRoundRawDto> findRetentionRounds(@Param("req") RetentionRoundSearchRequestDto req);
+
+    // 회차 목록 총 개수 조회 (페이징용)
+    long countRetentionRounds(@Param("req") RetentionRoundSearchRequestDto req);
+}
