b5bbf0b62d79a2bda46b7eec6ca6201811a48856
Taein5415 <kskk5050@naver.com>
2025-06-25 10:51:28 +0900

    [test #210] 부서 상세 조회 테스트코드 작성

--- momentum-dao-be/src/main/java/com/dao/momentum/organization/department/query/dto/response/DepartmentDetailDTO.java
+++ momentum-dao-be/src/main/java/com/dao/momentum/organization/department/query/dto/response/DepartmentDetailDTO.java
@@ -1,5 +1,7 @@
 package com.dao.momentum.organization.department.query.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,6 +9,8 @@ import java.time.LocalDate;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class DepartmentDetailDTO {
     private String name;
     private String contact;
--- momentum-dao-be/src/main/java/com/dao/momentum/organization/employee/query/dto/response/DepartmentMemberDTO.java
+++ momentum-dao-be/src/main/java/com/dao/momentum/organization/employee/query/dto/response/DepartmentMemberDTO.java
@@ -1,10 +1,14 @@
 package com.dao.momentum.organization.employee.query.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class DepartmentMemberDTO {
     private String name;
     private String position;
