f2b25df4b5afbf12f3398829f3c8b723ccd4de50
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-06-26 08:49:58 +0900

    [refactor] spring security 설정 추가

--- momentum-dao-be/src/main/java/com/dao/momentum/work/query/controller/WorkQueryController.java
+++ momentum-dao-be/src/main/java/com/dao/momentum/work/query/controller/WorkQueryController.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,7 +38,6 @@ public class WorkQueryController {
     }
 
     @GetMapping
-    @PreAuthorize("hasAnyAuthority('MASTER', 'HR_MANAGER')")
     @Operation(summary = "관리자 출퇴근 조회", description = "관리자가 특정 사원의 출퇴근 내역을 조회합니다.")
     public ResponseEntity<ApiResponse<WorkListResponse>> getWorks(
             @ModelAttribute AdminWorkSearchRequest adminWorkSearchRequest
