a37d75ab2f6a148d02e7682bcdae32a4d222a4c0
Taein5415 <kskk5050@naver.com>
2025-06-25 10:18:36 +0900

    [feat #210] 부서 상세 정보 조회 기능 구현

--- momentum-dao-be/src/main/java/com/dao/momentum/organization/department/query/controller/DepartmentQueryController.java
+++ momentum-dao-be/src/main/java/com/dao/momentum/organization/department/query/controller/DepartmentQueryController.java
@@ -1,6 +1,7 @@
 package com.dao.momentum.organization.department.query.controller;
 
 import com.dao.momentum.common.dto.ApiResponse;
+import com.dao.momentum.organization.department.query.dto.response.DepartmentDetailResponse;
 import com.dao.momentum.organization.department.query.dto.response.DepartmentsInfoResponse;
 import com.dao.momentum.organization.department.query.service.DepartmentQueryService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
