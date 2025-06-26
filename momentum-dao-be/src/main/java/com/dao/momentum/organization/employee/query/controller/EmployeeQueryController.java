f55196d0cc854f44c49b253761590e9bc3c89b38
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-06-26 11:08:27 +0900

    [feat #115] 사원 개인 정보 조회 구현

--- momentum-dao-be/src/main/java/com/dao/momentum/organization/employee/query/controller/EmployeeQueryController.java
+++ momentum-dao-be/src/main/java/com/dao/momentum/organization/employee/query/controller/EmployeeQueryController.java
@@ -1,11 +1,36 @@
 package com.dao.momentum.organization.employee.query.controller;
 
-import com.dao.momentum.organization.employee.query.service.EmployeeQueryService;
+import com.dao.momentum.common.dto.ApiResponse;
+import com.dao.momentum.organization.employee.query.dto.response.EmployeeDetailsResponse;
+import com.dao.momentum.organization.employee.query.service.AdminEmployeeQueryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/employees")
 @RequiredArgsConstructor
+@Tag(name = "사원 정보 조회", description = "사원 본인 정보 조회 API")
 public class EmployeeQueryController {
-    private final EmployeeQueryService employeeService;
+    private final AdminEmployeeQueryService adminEmployeeQueryService;
+
+    @GetMapping("/me")
+    @Operation(summary = "사원 개인 정보 조회", description = "사원이 본인 정보를 조회합니다.")
+    public ResponseEntity<ApiResponse<EmployeeDetailsResponse>> getEmployeeDetails(
+            @AuthenticationPrincipal UserDetails userDetails
+            ) {
+        long empId = Long.parseLong(userDetails.getUsername());
+
+        return ResponseEntity.ok(
+                ApiResponse.success(
+                        adminEmployeeQueryService.getEmployeeDetails(empId)
+                )
+        );
+    }
 }
