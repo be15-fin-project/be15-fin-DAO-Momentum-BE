98ad52403cf2d1f14c63da0cc2b099002cdc95f5
splguyjr <splguyjr@naver.com>
2025-07-03 19:51:57 +0900

    [chore #323] 배치 서버 호출을 위한 의존성(OpenFeign) 및 docker-compose yml 작성

--- momentum-dao-be/src/main/java/com/dao/momentum/MomentumDaoBeApplication.java
+++ momentum-dao-be/src/main/java/com/dao/momentum/MomentumDaoBeApplication.java
@@ -2,10 +2,12 @@ package com.dao.momentum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableFeignClients(basePackages = "com.dao.momentum.notification.command.application.service")
 public class MomentumDaoBeApplication {
 
     public static void main(String[] args) {
