a131d40d47493f91bc617104c39d89c1842c064f
meowdule <meowdule73@gmail.com>
2025-07-03 11:09:57 +0900

    [refactor] 인사 평가 이의제기 상세 조회 수정 및 인사 평가 이의제기 요청 상세 조회 추가

--- momentum-dao-be/src/main/java/com/dao/momentum/evaluation/hr/query/service/HrObjectionQueryService.java
+++ momentum-dao-be/src/main/java/com/dao/momentum/evaluation/hr/query/service/HrObjectionQueryService.java
@@ -2,10 +2,14 @@ package com.dao.momentum.evaluation.hr.query.service;
 
 import com.dao.momentum.evaluation.hr.query.dto.request.HrObjectionListRequestDto;
 import com.dao.momentum.evaluation.hr.query.dto.response.HrObjectionListResultDto;
+import com.dao.momentum.evaluation.hr.query.dto.response.ObjectionDetailResultDto;
 
 public interface HrObjectionQueryService {
     /**
      * 팀장이 작성한 사원의 인사 평가 이의제기 목록 조회
      */
     HrObjectionListResultDto getObjections(HrObjectionListRequestDto req);
+
+    // 인사 평가 이의 제기 상세 조회
+    ObjectionDetailResultDto getObjectionDetail(Long objectionId);
 }
