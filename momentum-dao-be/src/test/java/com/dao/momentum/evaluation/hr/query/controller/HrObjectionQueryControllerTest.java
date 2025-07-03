001dde79b4c38bdb482e02ac844591a3a28b15e4
meowdule <meowdule73@gmail.com>
2025-07-03 11:21:12 +0900

    [test #318] 인사 평가 이의제기 요청 상세 조회 기능 테스트 코드 구현

--- momentum-dao-be/src/test/java/com/dao/momentum/evaluation/hr/query/controller/HrObjectionQueryControllerTest.java
+++ momentum-dao-be/src/test/java/com/dao/momentum/evaluation/hr/query/controller/HrObjectionQueryControllerTest.java
@@ -3,8 +3,7 @@ package com.dao.momentum.evaluation.hr.query.controller;
 import com.dao.momentum.common.dto.Pagination;
 import com.dao.momentum.common.dto.Status;
 import com.dao.momentum.evaluation.hr.query.dto.request.HrObjectionListRequestDto;
-import com.dao.momentum.evaluation.hr.query.dto.response.HrObjectionItemDto;
-import com.dao.momentum.evaluation.hr.query.dto.response.HrObjectionListResultDto;
+import com.dao.momentum.evaluation.hr.query.dto.response.*;
 import com.dao.momentum.evaluation.hr.query.service.HrObjectionQueryService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
@@ -78,6 +77,47 @@ class HrObjectionQueryControllerTest {
                 .andExpect(jsonPath("$.data.list[0].status").value("PENDING"))
                 .andExpect(jsonPath("$.data.list[0].score").value(85))
                 .andExpect(jsonPath("$.data.pagination.totalItems").value(1))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("이의제기 상세 조회 성공")
+    @WithMockUser(username = "1", roles = "MANAGER")
+    void getObjectionDetail_success() throws Exception {
+        // given
+        Long objectionId = 5001L;
+        ObjectionItemDto itemDto = ObjectionItemDto.builder()
+                .objectionId(objectionId)
+                .resultId(100L)
+                .empNo("20240001")
+                .empName("김현우")
+                .build();
+
+        List<FactorScoreDto> factorScores = List.of(
+                new FactorScoreDto("업무 수행 역량", "우수"),
+                new FactorScoreDto("협업 역량", "보통")
+        );
+
+        WeightInfo weightInfo = new WeightInfo(20, 20, 20, 20, 10, 10);
+        RateInfo rateInfo = new RateInfo(10, 20, 30, 30, 10);
+
+        ObjectionDetailResultDto responseDto = ObjectionDetailResultDto.builder()
+                .itemDto(itemDto)
+                .factorScores(factorScores)
+                .weightInfo(weightInfo)
+                .rateInfo(rateInfo)
+                .build();
+
+        given(service.getObjectionDetail(objectionId)).willReturn(responseDto);
+
+        // when & then
+        mockMvc.perform(get("/hr-objections/requests/{objectionId}", objectionId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data.itemDto.objectionId").value(5001))
+                .andExpect(jsonPath("$.data.factorScores", hasSize(2)))
+                .andExpect(jsonPath("$.data.weightInfo.weightPerform").value(20))
+                .andExpect(jsonPath("$.data.rateInfo.rateA").value(20))
                 .andDo(print());
     }
 }
