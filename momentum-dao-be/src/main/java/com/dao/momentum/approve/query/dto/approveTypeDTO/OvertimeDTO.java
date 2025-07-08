fbfa9a9a4086b8b0a245a1a183369e989a0bb056
devyujinjeong <devyujinjeong887@gmail.com>
2025-07-08 09:20:19 +0900

    [refactor] 초과 근무 종류를 확인하는 메소드 위치 수정

--- momentum-dao-be/src/main/java/com/dao/momentum/approve/query/dto/approveTypeDTO/OvertimeDTO.java
+++ momentum-dao-be/src/main/java/com/dao/momentum/approve/query/dto/approveTypeDTO/OvertimeDTO.java
@@ -23,25 +23,4 @@ public class OvertimeDTO {
 
     private List<String> workTypes = new ArrayList<>();
 
-    public void classifyWorkTypes() {
-        Set<String> types = new HashSet<>();
-
-        LocalDateTime cursor = startAt;
-        while (cursor.isBefore(endAt)) {
-            DayOfWeek day = cursor.getDayOfWeek();
-            int hour = cursor.getHour();
-
-            if (day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY) {
-                types.add("HOLIDAY");
-            } else if (hour >= 22 || hour < 6) {
-                types.add("NIGHT");
-            } else if (hour >= 18) {
-                types.add("OVERTIME");
-            }
-
-            cursor = cursor.plusMinutes(1);
-        }
-
-        this.workTypes = new ArrayList<>(types);
-    }
 }
