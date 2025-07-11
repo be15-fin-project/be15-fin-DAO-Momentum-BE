88beb63b7ce0b84fb04c61051dfe19fc4081cd91
devyujinjeong <devyujinjeong887@gmail.com>
2025-07-11 12:46:58 +0900

    [refactor] endpoint 변경 및 조회 관련 오류 수정

--- momentum-dao-be/src/main/java/com/dao/momentum/approve/query/controller/RemainingVacationQueryController.java
+++ momentum-dao-be/src/main/java/com/dao/momentum/approve/query/controller/RemainingVacationQueryController.java
@@ -8,13 +8,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/employees")
+@RequestMapping("/remaining")
 @Tag(name = "잔여 휴가 시간 조회", description ="잔여 휴가 시간 조회 API")
 @RequiredArgsConstructor
 public class RemainingVacationQueryController {
@@ -22,22 +23,26 @@ public class RemainingVacationQueryController {
     private final RemainingVacationQueryService remainingVacationQueryService;
 
     /* 잔여 연차 시간 조회 */
-    @GetMapping("/{empId}/dayoff")
+    @GetMapping("/dayoff")
     @Operation(summary = "잔여 연차 시간 조회", description ="사원의 잔여 연차 시간을 조회합니다.")
     public ResponseEntity<ApiResponse<DayoffResponse>> getRemainingDayoffs(
-            @PathVariable long empId
+            @AuthenticationPrincipal UserDetails userDetails
     ) {
+        long empId = Long.parseLong(userDetails.getUsername());
+
         DayoffResponse response = remainingVacationQueryService.getRemainingDayoffs(empId);
 
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
     /* 잔여 리프레시 휴가 일수 조회*/
-    @GetMapping("/{empId}/refresh")
+    @GetMapping("/refresh")
     @Operation(summary = "잔여 리프레시 휴가 일수 조회", description ="사원의 잔여 리프레시 휴가 일수를 조회합니다.")
     public ResponseEntity<ApiResponse<RefreshResponse>> getRemainingRefreshs(
-            @PathVariable long empId
+            @AuthenticationPrincipal UserDetails userDetails
     ) {
+        long empId = Long.parseLong(userDetails.getUsername());
+
         RefreshResponse response = remainingVacationQueryService.getRemainingRefreshs(empId);
 
         return ResponseEntity.ok(ApiResponse.success(response));
