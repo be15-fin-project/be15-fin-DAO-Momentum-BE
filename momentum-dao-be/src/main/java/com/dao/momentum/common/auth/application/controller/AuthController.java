22e437beb6920fd86b3261dd3ffbe5648173cf82
splguyjr <splguyjr@naver.com>
2025-07-14 16:39:05 +0900

    [feat] SameSite None 옵션 적용, cloudfront domain 수정

--- momentum-dao-be/src/main/java/com/dao/momentum/common/auth/application/controller/AuthController.java
+++ momentum-dao-be/src/main/java/com/dao/momentum/common/auth/application/controller/AuthController.java
@@ -117,20 +117,20 @@ public class AuthController {
     private ResponseCookie createRefreshTokenCookie(String refreshToken) {
         return ResponseCookie.from("refreshToken", refreshToken)
                 .httpOnly(true)
-                // .secure(true) // 운영 환경에서 HTTPS 사용 시 활성화
+                .secure(true) // 운영 환경에서 HTTPS 사용 시 활성화
                 .path("/")
                 .maxAge(Duration.ofDays(7))
-                .sameSite("Strict")
+                .sameSite("None")
                 .build();
     }
 
     private ResponseCookie createDeleteRefreshTokenCookie() {
         return ResponseCookie.from("refreshToken", "")
                 .httpOnly(true)
-                // .secure(true)
+                .secure(true)
                 .path("/")
                 .maxAge(0)
-                .sameSite("Strict")
+                .sameSite("None")
                 .build();
     }
 
